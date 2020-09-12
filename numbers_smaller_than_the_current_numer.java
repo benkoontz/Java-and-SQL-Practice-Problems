35b75610b48c03eebe8eab666b4ec33c1c358410
benkoontz <36342241+benkoontz@users.noreply.github.com>
2020-09-12 12:08:07 -0700

    Update numbers_smaller_than_the_current_numer.java

--- numbers_smaller_than_the_current_numer.java
+++ numbers_smaller_than_the_current_numer.java
@@ -1,6 +1,7 @@
 // given the array nums, for each nums[i] find out how many numbers in the array are smaller // // than it. That is, for each nums[i], count the number of valid j's such that j != i and / // // nums[j] < nums[i]
 
-// return the answer as an int array
+// example
+
 /* Input: nums = [8,1,2,2,3]
 Output: [4,0,1,1,3]
 Explanation: 
