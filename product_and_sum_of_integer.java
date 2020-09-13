a93e3c3aa05ed5d2ce32528a06000855801f0b0b
benkoontz <36342241+benkoontz@users.noreply.github.com>
2020-09-13 16:38:16 -0700

    Update product_and_sum_of_integer.java

--- product_and_sum_of_integer.java
+++ product_and_sum_of_integer.java
@@ -1,4 +1,4 @@
-// Given an integer n, return the difference between the product o its digits and the sum 
+// Given an integer n, return the difference between the product of its digits and the sum 
 // of it's digits
 
 /*
