628ce367087167908028ca704258dfcd9b3ecf97
benkoontz <36342241+benkoontz@users.noreply.github.com>
2020-09-13 17:43:21 -0700

    Create conver_string_to_lowercase.java

---
+++ conver_string_to_lowercase.java
@@ -0,0 +1,9 @@
+// given an input string str, conevrt the string to lower case
+
+class Solution {
+    public String toLowerCase(String str) {
+        
+      // return the string to lowercase
+      return str.toLowerCase();
+    }
+}
